cd328a37456d40eb364258c70c9927ca2beef979
dangtruongpd <dangtruongpd@gmail.com>
2020-07-09 18:05:42 +0700

    sua lai duong dan

--- controller/index.php
+++ controller/index.php
@@ -1,26 +1,26 @@
 <?php
 
-  include "./../view/header.php";
+  include "./view/header.php";
 
   $action = isset($_GET['act'])?($_GET['act']):'home';
   switch($action){
     case 'home':
-      include "./../view/home.php";
+      include "./view/home.php";
       break;
     case 'courses':
-      include "./../view/courses.php";
+      include "./view/courses.php";
       break;
     case 'signin':
-      include "./../view/signin.php";
+      include "./view/signin.php";
       break;
     case 'signup':
-      include "./../view/signup.php";
+      include "./view/signup.php";
       break;
     default: 
-      include "./../view/404.php";
+      include "./view/404.php";
       break;
   }
 
-  include "./../view/footer.php";
-  include "./../view/script.php";
+  include "./view/footer.php";
+  include "./view/script.php";
 ?>
