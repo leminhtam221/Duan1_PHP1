221ee31ddb4e5033593ea7681543a7cffa00138d
dangtruongpd <dangtruongpd@gmail.com>
2020-07-12 19:34:12 +0700

    them view

--- controller/signin.php
+++ controller/signin.php
@@ -1,15 +1,2 @@
 <?php
-$signin_error = false;
-if(isset($_POST['uname'])){
-   $user = login($_POST['uname'],$_POST['pwd']);
-   // $value = array($user['id'],$user['tai_khoan'],$user['ho_ten'],$user['email']);
-   // if($user['id']>0){
-   //    setcookie('user', $value, time() + (86400 * 180), "/");
-      
-   // }else{
-   //    $signin_error = true;
-   // }
-   var_dump($user);
-}
-
 include './view/signin.php';
--- model/signin.php
+++
@@ -1,5 +0,0 @@
-<?php
-function login($uname,$pwd){
-   $user = find("select * from user where tai_khoan=$uname and mat_khau=$pwd");
-   return $user;
-}
