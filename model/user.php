887f934570bfcf4b3a002ef961c7092e5e31e97a
dangtruongpd <dangtruongpd@gmail.com>
2020-07-15 00:11:23 +0700

    them chuc nang giang vien dang ky

--- model/user.php
+++ model/user.php
@@ -3,7 +3,7 @@ function checkLogin($uname, $pwd){
    $user = find("select * from user where tai_khoan='$uname' and mat_khau='$pwd' or email='$uname' and mat_khau='$pwd'");
    return $user;
 }
-function signUp($uname, $pwd, $email, $fname, $lname){
+function userSignUp($uname, $pwd, $email, $fname, $lname){
    $sql = "insert into user(tai_khoan, mat_khau, email, ho_ten) values('$uname','$pwd','$email','$fname $lname')";
    execSQL($sql,0);
 }
---
+++ view/contact.php
@@ -0,0 +1,103 @@
+<div class="page-banner">           			
+        <div class="hvrbox">
+            <img src="./view/base/images/bn.jpg" alt="Mountains" class="hvrbox-layer_bottom">
+            <div class="hvrbox-layer_top">
+                <div class="container">
+                    <div class="overlay-text text-left">						
+                        <h3>Contact</h3>
+                        <nav aria-label="breadcrumb">
+                          <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Conatct</li>
+                          </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>	                     
+    </div>
+ 
+	<div class="contact-us-1x">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="title-left">
+						<h2>Liên Hệ</h2>
+					</div>	
+				</div>
+				<div class="col-md-7">
+					<div class="contact-form">
+						<form>
+							<div class="row">
+								<div class="col-md-12">	
+									<div class="single-input">																  
+										<div class="form-group">
+											<input type="text" class="form-control" placeholder="Tên*" aria-label="Name">				    										    						    
+										</div>
+									</div>
+								</div>	
+								<div class="col-md-12">
+									<div class="single-input">																  
+										<div class="form-group">							    
+										    <input type="email" class="form-control" placeholder="Email*">							    
+										</div>
+								  	</div>
+								</div>  		
+								<div class="col-md-12">	
+									<div class="single-input">							  
+										<div class="form-group">										
+											<textarea class="form-control" placeholder="Thông Điệp*" rows="5"></textarea>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-12">																	
+									<a href="#" class="btn-small"> Gửi Ngay </a>								
+								</div>
+							</div>
+
+						</form>
+					</div>
+				</div>
+				<div class="col-md-4 offset-md-1">
+					<div class="contact-address">
+						<ul>
+                       		<li>
+								<div class="media">
+									<img src="./view/base/images/location.png" alt="Mountains">
+								  <div class="media-body">
+									QTSC Building 1, Công viên phần mềm Quang Trung, P. Tân Chánh Hiệp, Q.12, TP.HCM, Việt Nam
+								  </div>
+								</div>
+                       		</li>
+                       		<li>
+								<div class="media">
+									<img src="./view/base/images/phone.png" alt="Mountains">
+								  <div class="media-body">
+									+84 398623696<br>
+								  </div>
+								</div>
+                       		</li>
+                       		<li>
+								<div class="media">
+									<img src="./view/base/images/message.png" alt="Mountains">
+								  <div class="media-body">
+									leminhtam@info.com
+								  </div>
+								</div>
+                       		</li>													
+						</ul>
+
+						<div class="footer-social-link">
+							<ul>
+								<li><a href="#"> <i class="fab fa-facebook-f"></i> </a></li>	
+								<li><a href="#"> <i class="fab fa-instagram"></i> </a></li>		
+								<li><a href="#"> <i class="fab fa-twitter"></i> </a></li>
+								<li><a href="#"> <i class="fab fa-google-plus-g"></i> </a></li>
+							</ul>					
+						</div>
+					</div>
+				</div>
+													
+			</div>
+		</div>
+	</div> 
