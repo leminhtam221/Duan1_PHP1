47b5ee723eaf04749c90cf1e0898a94b6989e6b0
leminhtam221 <le.minhtam221@gmail.com>
2020-07-15 15:56:11 +0700

    load data trang detail

--- model/giang-vien.php
+++ model/giang-vien.php
@@ -3,11 +3,18 @@
     $sql = "select ho_ten from giang_vien where id=".$idGiangVien;
     return find($sql);
   }
+  
+  function layThongTinGiangVien($idGiangVien){
+    $sql = "select * from giang_vien where id=".$idGiangVien;
+    return find($sql);
+  }
+
   function getLecture(){
     $sql = "select tai_khoan from giang_vien";
     $res = findMultiple($sql);
     return $res;
   }
+
   function lectureSignUp($uname, $pwd, $email, $fname, $lname){
     $sql = "insert into giang_vien(tai_khoan, mat_khau, email, ho_ten) values('$uname','$pwd','$email','$fname $lname')";
     execSQL($sql,0);
