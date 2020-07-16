4b89fc18a9b36e2035489aba03681676e37a44fb
dangtruongpd <dangtruongpd@gmail.com>
2020-07-17 01:42:55 +0700

    bo sung phan cookie login, chinh lai phan comment

--- controller/controller.php
+++ controller/controller.php
@@ -10,44 +10,6 @@
     }
   }
   include "./controller/".$controller.".php";
-  // $action = isset($_GET['act'])?($_GET['act']):'trang-chu';
-  // switch($action){
-  //   case 'trang-chu':
-  //     include "./controller/home.php";
-  //     break;
-  //   case 'khoa-hoc':
-  //     include "./controller/courses.php";
-  //     break;
-  //   case 'khoa-hoc-chi-tiet':
-  //     include "./controller/course-detail.php";
-  //     break;
-  //   case 'binh-luan':
-  //     include "./controller/binh-luan.php";
-  //     break;
-  //   case 'dang-nhap':
-  //     include "./controller/signin.php";
-  //     break;
-  //   case 'dang-ky':
-  //     include "./controller/signup.php";
-  //     break;
-  //   case 'dang-xuat':
-  //     break;
-  //   case 'thong-tin':
-  //     include "./controller/about.php";
-  //     break;
-  //   case 'bai-viet':
-  //     include "./controller/blog.php";
-  //     break;
-  //   case 'lien-he':
-  //     include "./controller/contact.php";
-  //     break;
-  //   case 'gio-hang':
-  //     include "./controller/cart.php";
-  //     break;
-  //   default: 
-  //     include "./view/404.php";
-  //     break;
-  // }
 
   include "./view/footer.php";
   include "./view/base/script.php";
